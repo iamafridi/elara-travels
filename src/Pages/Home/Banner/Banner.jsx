da6a093ebaa8f9826d2e3f80fc088ecf49f4501a
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-12-03 19:22:50 +0600

    created Banner section using react carousel

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,40 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import banner1 from '../../../assets/Banner/banner1.jpg'
+import banner2 from '../../../assets/Banner/banner2.jpg'
+import banner4 from '../../../assets/Banner/banner4.jpg'
+import banner5 from '../../../assets/Banner/banner5.jpg'
+import banner6 from '../../../assets/Banner/banner6.jpeg'
+import banner7 from '../../../assets/Banner/banner7.jpg'
+import banner8 from '../../../assets/Banner/banner8.jpg'
+
+const Banner = () => {
+    return (
+        <Carousel>
+            <div>
+                <img src={banner1} />
+            </div>
+            <div>
+                <img src={banner2} />
+            </div>
+            <div>
+                <img src={banner4} />
+            </div>
+            <div>
+                <img src={banner5} />
+            </div>
+            <div>
+                <img src={banner6} />
+            </div>
+            <div>
+                <img src={banner7} />
+            </div>
+            <div>
+                <img src={banner8} />
+            </div>
+        </Carousel>
+    );
+};
+
+export default Banner;
