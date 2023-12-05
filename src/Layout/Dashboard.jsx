0c17a8d3ea779cd3b10de893d899ffdb73634657
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-12-05 16:30:16 +0600

    Deleted and synced Data using fetch

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,7 +1,9 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
         <div className="flex">
             <div className="w-64 min-h-screen bg-gray-600">
@@ -13,7 +15,7 @@ const Dashboard = () => {
                         <NavLink to="/dashboard/reservation"> <FaCalendar /> Reservation</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/dashboard/cart"> <FaShoppingCart /> My Cart</NavLink>
+                        <NavLink to="/dashboard/cart"> <FaShoppingCart /> My Cart ({cart.length})</NavLink>
                     </li>
                     <li>
                         <NavLink to="/dashboard/review"> <FaAd />Add a  Review</NavLink>
@@ -21,10 +23,17 @@ const Dashboard = () => {
                     <li>
                         <NavLink to="/dashboard/bokings"> <FaList /> Bookings</NavLink>
                     </li>
+                    <div className="divider"></div>
+                    <li>
+                        <NavLink to="/"> <FaHome /> Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/book/Mountain"> <FaSearch />Book Trip</NavLink>
+                    </li>
                 </ul>
             </div>
 
-            <div className="flex-1">
+            <div className="flex-1 p-10">
                 <Outlet></Outlet>
             </div>
         </div>
--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -1,8 +1,92 @@
+import { FaTrash, FaTrashAlt } from "react-icons/fa";
+import useCart from "../../../hooks/useCart";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const Cart = () => {
+    const [cart, refetch] = useCart();
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+
+    const axiosSecure = useAxiosSecure();
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/carts/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Tour has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+
+            }
+        });
+    }
+
     return (
         <div>
-            <h2 className="text-6xl">MY CART</h2>
+            <div className="flex justify-evenly mb-8">
+                <h2 className="text-3xl">Tour Bookings : {cart.length}</h2>
+                <h2 className="text-3xl">Due : {totalPrice}</h2>
+                <button className="btn btn-primary">Pay</button>
+            </div>
+            <div className="overflow-x-auto ">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                Tour Id
+                            </th>
+                            <th>Image</th>
+                            <th>Tour Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map(item => <tr key={item._id}>
+                                <th>
+                                    {item._id}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image_url} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td>${item.price}</td>
+                                <th>
+                                    <button
+                                        onClick={() => handleDelete(item._id)}
+                                        className="btn btn-ghost btn-lg"><FaTrashAlt /></button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
